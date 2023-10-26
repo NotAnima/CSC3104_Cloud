25623486ae46e2c5aa4d0bac7204b57baf0ecdfd
EmpyreaLiv <2200795@sit.singaporetech.edu.sg>
2023-10-26 14:13:05 +0800

    Changed file directory and changed RF model to logistic regression model

--- rpc/Model.py
+++ rpc/Model.py
@@ -1,8 +1,8 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 import pickle
 
 def read_data(path):
@@ -21,24 +21,30 @@ def load_model(data):
     y = data['Diabetes_012']
 
     # Split the data into training and testing sets (80% train, 20% test), stratify to ensure equal proportions of class 0,1,2 diabetes indicator for the test set
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Initialize the Random Forest Classifier with controlled parameters for faster training
-    rf_model = RandomForestClassifier(n_estimators=10, max_depth=10, random_state=42, class_weight='balanced')
-    rf_model.fit(X_train, y_train)
+    log_reg_model = LogisticRegression(random_state=42, max_iter=1000)
+    log_reg_model.fit(X_train, y_train)
 
-    return rf_model
+    return log_reg_model
 
-    # y_pred_rf = rf_model.predict(X_test)
+    # # Make predictions on the test set
+    # y_pred = log_reg_model.predict(X_test)
 
-    # classification_metrics_rf = classification_report(y_test, y_pred_rf)
-    # conf_matrix_rf = confusion_matrix(y_test, y_pred_rf)
+    # # Calculate the model performance
+    # accuracy = accuracy_score(y_test, y_pred)  # for classification
+    # classification_rep = classification_report(y_test, y_pred)
+    # conf_matrix = confusion_matrix(y_test, y_pred)
+
+    # print("Accuracy")
+    # print(accuracy)
 
     # print("Classification Report:")
-    # print(classification_metrics_rf)
+    # print(classification_rep)
 
     # print("Confusion Matrix:")
-    # print(conf_matrix_rf)
+    # print(conf_matrix)
 
 def save_model(model):
     with open("rpc/model.pkl", "wb") as file:
