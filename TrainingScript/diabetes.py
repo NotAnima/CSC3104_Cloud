b9174657397282883d08acbc7781ccdb1302ad33
Jonnnana <joshua.981@live.com>
2023-11-02 17:05:32 +0800

    Fixed small issue

--- TrainingScript/diabetes.py
+++ TrainingScript/diabetes.py
@@ -106,6 +106,7 @@ def train_base_model(average_weights, average_biases, shape):
     weights = np.array(average_weights).reshape(shape)
     bias = np.array(average_biases)
     model = LogisticRegression()
+    model.classes_ = np.array([0,1])
     model.coef_ = weights
     model.intercept_ = bias
     return model
