ba100c14bd1952bf07e9139820506415b5bbc4b9
Dereknan <50665229+Dereknan@users.noreply.github.com>
2023-11-07 15:32:41 +0800

    Minor adjustments

---
+++ Server/init.sql
@@ -0,0 +1,21 @@
+CREATE TABLE patient (
+    id SERIAL PRIMARY KEY,
+    high_bp REAL NOT NULL, -- 0 for Low Blood Pressure, 1 for High Blood Pressure
+    high_chol REAL NOT NULL, -- 0 for No High Cholesterol, 1 for High Cholesterol
+    chol_check REAL NOT NULL, -- 0 for Not in the last 5 years, 1 for Yes, within the last 5 years
+    bmi REAL NOT NULL CHECK (bmi >= 0.0 AND bmi <= 251.1), -- BMI
+    smoker REAL NOT NULL, -- 0 for No, 1 for Yes
+    stroke REAL NOT NULL, -- 0 for No, 1 for Yes
+    heart_disease_or_attack REAL NOT NULL, -- 0 for No, 1 for Yes
+    phys_activity REAL NOT NULL, -- 0 for No, 1 for Yes
+    fruits REAL NOT NULL, -- 0 for No, 1 for Yes
+    veggies REAL NOT NULL, -- 0 for No, 1 for Yes
+    hvy_alcohol_consump REAL NOT NULL, -- 0 for No, 1 for Yes
+    phys_hlth REAL NOT NULL,
+    diff_walk REAL NOT NULL, -- 0 for No, 1 for Yes
+    sex REAL NOT NULL, -- 0 for Female, 1 for Male
+    age REAL NOT NULL, -- Age group, represented by numbers 1 through 13
+    label REAL NULL, -- doctors answer is here
+    labelled BOOL NOT NULL, -- Whether or not it should be displayed on doctors hub
+    sent BOOL NOT NULL -- indicates whether this row has been sent to the server already or not
+);
