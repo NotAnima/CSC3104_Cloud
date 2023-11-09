0601fe8737fd055abd0683784e249ccb2315bce2
Dereknan <50665229+Dereknan@users.noreply.github.com>
2023-11-09 20:39:49 +0800

    Minor fix for environment variable

--- TestResults/test.py
+++ TestResults/test.py
@@ -1,21 +1,21 @@
-import diabetes, random
+import diabetes
+import random
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
-from os import environ
+import os
 
 def read_data(path):
     file_path = path
     data = pd.read_csv(file_path)
-
     return data
 
-if (environ['MODEL'] != None):
-    modelFile = environ['MODEL']
+modelFile = os.environ.get('MODEL')
+
+if modelFile:
     fileLocation = "/model/" + modelFile
 else:
     fileLocation = "model.pkl"
-
 
 data = read_data("diabetes_15_columns.csv")
 model = diabetes.load_model(fileLocation)
@@ -30,5 +30,3 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # answer = model.predict(X_test)
 diabetes.test_model(model, X_test, y_test)
-
-
